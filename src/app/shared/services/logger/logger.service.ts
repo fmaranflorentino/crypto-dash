2a206e69acea44383967b34faef5628be68e5276
WizarBr <florentino.flavioweb@gmail.com>
2019-07-23 14:52:42 -0300

    logger service

--- src/app/shared/services/logger/logger.service.ts
+++ src/app/shared/services/logger/logger.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class LoggerService {
 
   constructor() { }
+
+  logError(message: string, stack: string) {
+    console.log('Throw Error: ' + message);
+  }
 }
