6df7dbd2f6920c348b19085f9db9ee5869fc7e73
WizarBr <florentino.flavioweb@gmail.com>
2019-07-23 14:52:18 -0300

    error service

--- src/app/shared/services/error/error.service.ts
+++ src/app/shared/services/error/error.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,23 @@ import { Injectable } from '@angular/core';
 export class ErrorService {
 
   constructor() { }
+
+  getClientMessage(error: Error): string {
+    if (!navigator.onLine) {
+      return 'No Internet Connection';
+    }
+    return error.message ? error.message : error.toString();
+  }
+
+  getClientStack(error: Error): string {
+    return error.stack;
+  }
+
+  getServerMessage(error: HttpErrorResponse): string {
+    return error.message;
+  }
+
+  getServerStack(error: HttpErrorResponse): string {
+    return 'stack';
+  }
 }
