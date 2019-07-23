b7e8284787b46a4d48e50e0b19610a8000eca8a2
WizarBr <florentino.flavioweb@gmail.com>
2019-07-23 14:51:35 -0300

    api service

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { BaseComponent } from './components/base/base.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent, BaseComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   exports: [
     BaseComponent,
-    HeaderComponent
+    HeaderComponent,
+    HttpClientModule
   ]
 })
 export class SharedModule { }
